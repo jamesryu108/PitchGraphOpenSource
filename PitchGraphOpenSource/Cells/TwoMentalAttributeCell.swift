9f86fe94298c1ba8c3fcc85d1ae0a699427173ee
James Ryu <jamesryu14@gmail.com>
2024-01-15 16:55:29 -0500

    PIT-05: Add stats-vc

---
+++ PitchGraphOpenSource/Cells/TwoMentalAttributeCell.swift
@@ -0,0 +1,207 @@
+//
+//  TwoMentalAttributeCell.swift
+//  PitchGraphOpenSource
+//
+//  Created by James Ryu on 2024-01-15.
+//
+
+import UIKit
+
+/// A cell used for displaying two mental attributes of a player.
+final class TwoMentalAttributeCell: UICollectionViewCell, FontAdjustable, ReusableCellConfiguring {
+    
+    // MARK: - Constants
+    
+    private enum Constants {
+        static let labelMinFontSize: CGFloat = 10
+        static let labelMaxFontSize: CGFloat = 20
+        static let leftPadding: CGFloat = 8
+        static let rightPadding: CGFloat = 8
+        static let widthMultiplier: CGFloat = 0.25
+        static let backgroundColorEven: UIColor = .systemBackground
+        static let backgroundColorOdd: UIColor = .systemGray6
+    }
+    
+    // MARK: - Properties
+    
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+    
+    // MARK: - UI Elements
+    
+    private let player1ScoreLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .right
+        label.isAccessibilityElement = true
+        label.accessibilityHint = "Displays the first player's mental score".localized
+        return label
+    }()
+    
+    private let attributeNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.isAccessibilityElement = true
+        label.accessibilityHint = "Displays the name of the mental attribute".localized
+        return label
+    }()
+    
+    private let player2ScoreLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.isAccessibilityElement = true
+        label.accessibilityHint = "Displays the second player's mental score".localized
+        return label
+    }()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setupFontSizeAdjustmentObserver()
+        adjustFontsSize()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Cell Configuration
+    
+    private func setupUI() {
+
+        contentView.addSubviews(player1ScoreLabel, attributeNameLabel, player2ScoreLabel)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        player1ScoreLabel.anchor(
+            left: contentView.leftAnchor,
+            horizontalLeftSpace: Constants.leftPadding,
+            width: contentView.frame.width * Constants.widthMultiplier,
+            centerY: contentView.centerYAnchor
+        )
+
+        attributeNameLabel.anchor(
+            left: player1ScoreLabel.rightAnchor,
+            horizontalLeftSpace: Constants.leftPadding,
+            right: player2ScoreLabel.leftAnchor,
+            horizontalRightSpace: Constants.rightPadding,
+            centerY: contentView.centerYAnchor
+        )
+        
+        player2ScoreLabel.anchor(
+            right: contentView.rightAnchor,
+            horizontalRightSpace: Constants.rightPadding,
+            width: contentView.frame.width * Constants.widthMultiplier,
+            centerY: contentView.centerYAnchor
+        )
+    }
+        
+    /// Configures the cell with player scores and an attribute name.
+    /// - Parameters:
+    ///   - player1Score: The score of player 1.
+    ///   - attributeName: The name of the attribute.
+    ///   - player2Score: The score of player 2.
+    ///   - index: The index of the cell.
+    func configure(player1Score: Int, attributeName: String, player2Score: Int, index: Int, option: Int) {
+        player1ScoreLabel.text = "\(player1Score)"
+        attributeNameLabel.text = attributeName.localized
+        player2ScoreLabel.text = "\(player2Score)"
+        
+        let isEvenRow = index % 2 != 0
+
+        let backgroundColor = isEvenRow ? Constants.backgroundColorEven : Constants.backgroundColorOdd
+        
+        player1ScoreLabel.backgroundColor = backgroundColor
+        attributeNameLabel.backgroundColor = backgroundColor
+        player2ScoreLabel.backgroundColor = backgroundColor
+        contentView.backgroundColor = backgroundColor
+
+        player1ScoreLabel.textColor = colorForScore(player1Score, option: option)
+        player2ScoreLabel.textColor = colorForScore(player2Score, option: option)
+    }
+
+    // MARK: - Text Color Calculation
+    
+    private func colorForScore(_ score: Int, option: Int) -> UIColor {
+        switch option {
+        case 0:
+            switch score {
+            case 0...9: return .systemRed
+            case 10...14: return .label
+            case 15...17: return .systemYellow
+            case 18...20: return .systemBlue
+            default: return .label
+            }
+        case 1:
+            switch score {
+            case 0...9: return .label
+            case 10...14: return Option2ColorTheme.bronze
+            case 15...17: return Option2ColorTheme.silver
+            case 18...20: return Option2ColorTheme.gold
+            default: return .label
+            }
+        case 2:
+            switch score {
+            case 0...9: return .systemGray
+            case 10...14: return .label
+            case 15...17: return .systemYellow
+            case 18...20: return .systemBlue
+            default: return .label
+            }
+        case 3:
+            switch score {
+            case 0...9: return .gray
+            case 10...14: return .systemYellow
+            case 15...17: return .systemBlue
+            case 18...20: return .systemOrange
+            default: return .label
+            }
+        default:
+            return .label
+        }
+    }
+    
+    // MARK: - Font Adjustment
+    
+    /// Adjusts the font sizes based on dynamic type settings.
+    func adjustFontsSize() {
+        adjustFontSize(
+            for: player1ScoreLabel,
+            minFontSize: Constants.labelMinFontSize,
+            maxFontSize: Constants.labelMaxFontSize,
+            forTextStyle: .body,
+            isBlackWeight: true
+        )
+        adjustFontSize(
+            for: attributeNameLabel,
+            minFontSize: Constants.labelMinFontSize,
+            maxFontSize: Constants.labelMaxFontSize,
+            forTextStyle: .body,
+            isBlackWeight: false
+        )
+        adjustFontSize(
+            for: player2ScoreLabel,
+            minFontSize: Constants.labelMinFontSize,
+            maxFontSize: Constants.labelMaxFontSize,
+            forTextStyle: .body,
+            isBlackWeight: true
+        )
+    }
+    
+    // MARK: - Font Size Adjustment Observer
+    
+    /// Sets up an observer to adjust font sizes when the content size category changes.
+    func setupFontSizeAdjustmentObserver() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(adjustFontsSize),
+                                               name: UIContentSizeCategory.didChangeNotification,
+                                               object: nil)
+    }
+}
--- PitchGraphOpenSource/ViewControllers/StatsViewController/PhysicalViewController.swift
+++ PitchGraphOpenSource/ViewControllers/StatsViewController/PhysicalViewController.swift
@@ -7,23 +7,130 @@
 
 import UIKit
 
-class PhysicalViewController: UIViewController {
+/// `PhysicalViewController` displays and manages a collection view of physical attributes for one or two players.
+final class PhysicalViewController: BaseCollectionViewController {
+ 
+    // MARK: - Properties
+
+    private var viewModel: PhysicalConfigurable
+    let statsMode: StatsMode
+    // MARK: - Initializers
+
+    /// Initializes a new instance of the view controller with given player data.
+    init(viewModel: PhysicalConfigurable, statsMode: StatsMode) {
+        self.viewModel = viewModel
+        self.statsMode = statsMode
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycle Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        processData()
+        setupUI()
+        setupBinding()
+    }
 
-        // Do any additional setup after loading the view.
+    // MARK: - Binding
+    private func setupBinding() {
+        viewModel.subscribeToSelectedOptionIndexChanges { [weak self] indexPath in
+            guard let self else {
+                return
+            }
+            DispatchQueue.main.async {
+                self.updateView()
+            }
+        }
     }
     
+    // MARK: - UI Setup Methods
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    /// Sets up UI components specific to `PhysicalViewController`.
+    private func setupUI() {
+        setupPhysicalCV()
     }
-    */
 
+    /// Configures the collection view for displaying physical attributes.
+    private func setupPhysicalCV() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(PhysicalAttributeCell.self, forCellWithReuseIdentifier: PhysicalAttributeCell.reuseIdentifier)
+        collectionView.register(TwoPhysicalAttributeCell.self, forCellWithReuseIdentifier: TwoPhysicalAttributeCell.reuseIdentifier)
+        collectionView.isScrollEnabled = true
+        collectionView.backgroundColor = self.statsMode == .appMode ? .systemBackground : .systemGray6
+    }
+
+    /// Processes the player data and maps it to the `attributes` array.
+    private func processData() {
+        viewModel.processData()
+    }
 }
+
+// MARK: - UICollectionViewDataSource
+
+extension PhysicalViewController {
+    
+    /// Returns the number of items in the collection view section.
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.physicalAttributesForPlayer1.count
+    }
+    
+    /// Provides the cell for each item in the collection view.
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let attribute1 = viewModel.physicalAttributesForPlayer1[indexPath.item]
+        
+        if let attribute2 = viewModel.physicalAttributesForPlayer2?[indexPath.item] {
+            // Cell configuration for two players' data.
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TwoPhysicalAttributeCell.reuseIdentifier, for: indexPath) as? TwoPhysicalAttributeCell else {
+                fatalError("Unable to dequeue TwoPhysicalAttributeCell")
+            }
+            
+            let player1Score = attribute1.score
+            let player2Score = attribute2.score
+            
+            cell.configure(
+                player1Score: player1Score,
+                attributeName: attribute1.name.capitalized,
+                player2Score: player2Score,
+                index: indexPath.row,
+                option: viewModel.selectedOptionIndex?.row ?? 0
+            )
+            
+            return cell
+        } else {
+            // Cell configuration for single player data.
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhysicalAttributeCell.reuseIdentifier, for: indexPath) as? PhysicalAttributeCell else {
+                fatalError("Unable to dequeue PhysicalAttributeCell")
+            }
+            
+            cell.configure(
+                with: attribute1,
+                index: indexPath.row,
+                option: viewModel.selectedOptionIndex?.row ?? 0
+            )
+            
+            return cell
+        }
+    }
+    
+    private func updateView() {
+        self.collectionView.reloadData()
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension PhysicalViewController {
+    
+    /// Specifies the size for each item in the collection view.
+    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 30)
+    }
+}
+
